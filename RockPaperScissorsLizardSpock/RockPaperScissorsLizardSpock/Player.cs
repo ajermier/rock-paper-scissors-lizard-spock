6c9b76dd843a42e4e4d59d3c441537f5610c9d08
ajermier <aaron.jermier@gmail.com>
2017-03-30 07:37:48 -0500

    final commit

--- RockPaperScissorsLizardSpock/RockPaperScissorsLizardSpock/Player.cs
+++ RockPaperScissorsLizardSpock/RockPaperScissorsLizardSpock/Player.cs
@@ -14,15 +14,7 @@ namespace RockPaperScissorsLizardSpock
         public string shootRound;
         public int shootRoundNum;
 
-        //constructors
-        public Player(string name, int score, string shootRound, int shootRoundNum)
-        {
-            this.name = GetPlayerName();
-            this.score = score;
-            this.shootRound = shootRound;
-            this.shootRoundNum = shootRoundNum;
-        }
-        
+        //constructors        
         public Player()
         {
 
